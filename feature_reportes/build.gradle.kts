6fba491ea8424de62bef8831564f425fe6e0e949
Luis Rocha <rocha.luis1710@gmail.com>
2026-07-26 22:46:12 -0500

    feat: actualizacion de estilos

--- feature_reportes/build.gradle.kts
+++ feature_reportes/build.gradle.kts
@@ -26,8 +26,8 @@ android {
 
     buildTypes {
         debug {
-            buildConfigField("String", "BASE_URL", "\"https://19crt6dl-3000.use.devtunnels.ms/\"")
-            buildConfigField("String", "SOCKET_URL", "\"https://19crt6dl-3000.use.devtunnels.ms/\"")
+            buildConfigField("String", "BASE_URL", "\"http://192.168.100.20:3000\"")
+            buildConfigField("String", "SOCKET_URL", "\"http://192.168.100.20:3000\"")
         }
         release {
             buildConfigField("String", "BASE_URL", "\"https://api.reportes.alertify.com/\"")
